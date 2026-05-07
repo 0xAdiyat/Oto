b1bd3ee06f4c6dd7ee6a9137b757fbb18b0a7cea
Ahsaf Hussain Adiyat <0xAdiyat@gmail.com>
2026-05-07 18:41:48 +0600

    Initial Commit

---
+++ Oto/OtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtoApp.swift
+//  Oto
+//
+//  Created by Yat on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct OtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
